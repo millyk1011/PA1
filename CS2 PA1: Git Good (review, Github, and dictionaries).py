be4fad1f97bbd3055c5bce2c05fed4cd4c996a49
millyk1011 <mkeene@madeira.org>
2025-11-17 19:34:45 -0500

    added a dictionary

---
+++ CS2 PA1: Git Good (review, Github, and dictionaries).py
@@ -0,0 +1,43 @@
+user_schedule = {
+    "mod 1" : {
+        "a block" : "",
+        "b block" : "",
+        "c block" : "",
+          },
+    "mod 2" : {
+        "a block" : "",
+        "b block" : "",
+        "c block" : "",
+          },
+    "mod 3" : {
+        "a block" : "",
+        "b block" : "",
+        "c block" : "",
+          },
+    "mod 4" : {
+        "a block" : "",
+        "b block" : "",
+        "c block" : "",
+          },,
+    "mod 5" : {
+        "a block" : "",
+        "b block" : "",
+        "c block" : "",
+          },
+    "mod 6" : {
+        "a block" : "",
+        "b block" : "",
+        "c block" : "",
+          },
+    "mod 7" : {
+        "a block" : "",
+        "b block" : "",
+        "c block" : "",
+          },,
+    "dblock" : {
+        "fall" : "",
+        "spring" : "",
+        "winter" : "",
+          }
+
+}
