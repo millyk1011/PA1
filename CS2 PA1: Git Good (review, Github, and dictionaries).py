39e3300677c1984034d3bc9a73730326b062611a
millyk1011 <mkeene@madeira.org>
2025-11-18 14:33:41 -0500

    added a d block option

--- CS2 PA1: Git Good (review, Github, and dictionaries).py
+++ CS2 PA1: Git Good (review, Github, and dictionaries).py
@@ -18,7 +18,7 @@ user_schedule = {
         "a block" : "",
         "b block" : "",
         "c block" : "",
-          },,
+          },
     "mod 5" : {
         "a block" : "",
         "b block" : "",
@@ -33,11 +33,74 @@ user_schedule = {
         "a block" : "",
         "b block" : "",
         "c block" : "",
-          },,
+          },
     "dblock" : {
         "fall" : "",
         "spring" : "",
         "winter" : "",
           }
-
 }
+
+def which_schedule():
+    result = input("Mod or D Block?\n> ")
+    if not result.strip(" ") in ["mod", "dblock", "d", "m"]:
+        print("Error! Please type correct name")
+        return False
+    else:
+        if result in ["mod", "m"]:            
+            return "mod"
+        else:            
+            return "dblock"
+    
+def verify_name(): #mod
+   
+    answer =  input("what mod are you scheduling, enter a number between 1 and 7? \n> ")
+    mod_name = "mod " + answer    
+    while mod_name.strip() not in user_schedule.keys():
+        print("error: between 1 and 7")
+        answer =  input("what mod are you scheduling, enter a number between 1 and 7? \n> ")
+        mod_name = "mod " + answer 
+    return True
+
+def dblock_name(): #dblock
+    name = input ("what is the name of your d block? \n>")
+    user_schedule["dblock"].update({name})
+
+def dblock_szn():
+    szn = input ("what is the season of your dblock\n>" )
+    while szn not in user_schedule["dblock"].keys():
+        print("error: pick between fall winter and spring")
+        szn = input ("what is the season of your dblock\n>")
+        answered = "dblock" + szn 
+    return True
+    
+def main():
+    print("Welcome")
+    option = which_schedule()  
+    
+    if option == False:
+        return  
+    
+    elif option == "mod": 
+       if not verify_name():
+            print("please try again")
+            return 
+       else:
+            print("great lets add a class\n")
+            
+    elif option == "dblock":
+        if not dblock_szn():
+            print("please say spring winter or fall")
+            return
+        elif not dblock_name():
+            print("d block added")
+    #print("great lets add a class\n")
+    # ask for a class
+    # ask which block
+    # put it in
+
+
+# main program     
+main()
+
+    
